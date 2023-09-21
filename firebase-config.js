64b7d3b828e87d373aca2f31f428e4aa5eb5c9a1
Niroj <nirojsedhai124@gmail.com>
2023-09-21 17:41:52 +0545

    Initial Setup

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+// export const storage = getStorage(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
